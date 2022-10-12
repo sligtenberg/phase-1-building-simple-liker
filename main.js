797180afc34cfdbbb0d4d0f973ada3ef8ee307fe
stephen ligtenberg <bountyhunter24@gmail.com>
2022-10-12 14:47:53 -0400

    commit tree 7e6f8936c30cff96dab588fb622ed5f6fde8b086

--- main.js
+++ main.js
@@ -3,9 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  //document.getElementById('modal').classList.add('hidden');
+  let likeGlyphs = document.getElementsByClassName('like-glyph');
+  for (let i = 0; i < likeGlyphs.length; i++) {
+    likeGlyphs[i].addEventListener('click', likeClickedFunc);
+  }
+});
 
-
-
+function likeClickedFunc(likeGlyphObj) {
+  mimicServerCall()
+  .then(successReponse => {
+    const heartObj = likeGlyphObj.target;
+    if (heartObj.classList.contains('activated-heart')) {
+      heartObj.classList.remove('activated-heart');
+    }
+    else {
+      heartObj.classList.add('activated-heart');
+    }
+  })
+  .catch(errorResponce => {
+    const errorMessage = document.getElementById('modal');
+    errorMessage.classList.remove('hidden');
+    errorMessage.children[1].textContent = errorResponce;
+    setTimeout(() => errorMessage.classList.add('hidden'), 3000);
+  });
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
